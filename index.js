af74833aac265e0350fd41bb11fe605fe2df4113
TanyaChichkan <tchichkan28@gmail.com>
2021-03-18 23:56:13 +0200

    delete done

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import { TaskCreator } from './js/main-classes/taskCreator.js';
 import { Modal } from './js/main-classes/modal.js';
 import { RenderList } from './js/main-classes/renderList.js';
+import {ListWrapper} from './js/main-classes/listWrapper.js';
 
 document.addEventListener('DOMContentLoaded', handler);
 
-const modal = new Modal();
-
-const task = new TaskCreator();
-const list = new RenderList();
 
 function handler() {
+    const modal = new Modal();
+    
+    const task = new TaskCreator();
+    const list = new RenderList();
     list.markUpCheck();
+    const wrapper = new ListWrapper();
 }
 
--- js/utils-classes/markUpCreator.js
+++ js/utils-classes/markUpCreator.js
@@ -6,5 +6,19 @@ export class MarkUpCreator {
     return newListElement;
   }
 
+  static  createListItems(arr) {
+    const markUp = arr.map(({ title, creationDate, expirationDate, done,className }, index) => {
+      return `<li class=${done? 'is-done' : 'list-item'} data-index=${index}>
+                        <input type="checkbox" name="task-done" value="done" data-index=${index} ${done && 'checked'} />
+                        <p class="list-text task-title">${title}</p>
+                        <p class="list-text">Start date: ${creationDate}</p>
+                        <p class="list-text">Finish date: ${expirationDate}</p>
+                        <p class="list-icon" data-name="task-delete" data-index=${index}>&#10006</p>
+                        <p class="list-icon">&#9998</p>
+                        </li>`;
+    });
+    return markUp.join(' ');
+  }
+
 
 }
